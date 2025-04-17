970c6f6b0274ba02c47d71e88969b0c9e3c488e8
James Futhey <kidGodzilla@users.noreply.github.com>
2025-04-17 22:08:30 +0800

    Update express.js

--- examples/express.js
+++ examples/express.js
@@ -52,6 +52,7 @@ app.use(express.static('clients'));
             baseUrl: 'http://127.0.0.1:5001',
             serveLoginPage: true,
             serveErrorPage: true,
+            redirectUris: ['http://localhost'],
             redirectUrl: '/',
 
             // Optional: Use Redis for session/state storage
